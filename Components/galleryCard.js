0699171a860f35da874604bbdd672a05a3bfc628
Noah Lynaugh <noahlynaugh@gmail.com>
2025-06-02 20:29:17 -0400

    minor changes to fix colors on gal card comp

--- Components/galleryCard.js
+++ Components/galleryCard.js
@@ -29,7 +29,7 @@ export class galleryCard extends HTMLElement{
 
     analyzeBrightness() {
     this.checkMediaBrightness(this.media).then(brightness => {
-        if (brightness < 185) {
+        if (brightness < 195) {
             this.buttonLink?.classList.add("lightCardButton");
             this.galleryText?.classList.add("lightCardText");
         }
